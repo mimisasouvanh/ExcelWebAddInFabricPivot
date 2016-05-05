1a6c100a08a1c6ea7763381ea3c347226aa1f463
mimisasouvanh <misaso@microsoft.com>
2016-05-05 11:52:21 -0700

    add UI changes per pivot tab switch

--- ExcelWebAddInFabricPivotWeb/Scripts/FabricUI/PivotTab.js
+++ ExcelWebAddInFabricPivotWeb/Scripts/FabricUI/PivotTab.js
@@ -17,14 +17,34 @@
             /** When clicking/tapping a link, select it. */
             $pivotContainer.on('click', '.ms-Pivot-link', function (event) {
                 event.preventDefault();
-                /** Check if current selection has elipses child element **/
+                /** Check if current selection has elipses child element */
                 var $elipsisCheck = $(this).find('.ms-Pivot-ellipsis');
 
-                /** Only execute when no elipses element can be found **/
+                /** Find pivot content linked to the current pivot */
+                var pivotContentSelector = $(this).attr('data-pivotContent');
+                var $pivotContent = $(pivotContentSelector);
+
+                /** Only execute when no elipses element can be found */
                 if ($elipsisCheck.length === 0) {
 
-                    $(this).siblings('.ms-Pivot-link').removeClass('is-selected');
+                    $(this).siblings('.ms-Pivot-link.is-selected').each(function (index, element) {
+
+                        /** Check if the pivot is attached to any content */
+                        /** Use data attribute as selector instead of forcing id/class */
+                        var contentSelector = $(element).attr('data-pivotContent');
+                        if (contentSelector) {
+
+                            /** Remove selection for pivot content */
+                            $(contentSelector).removeClass('is-selected');
+                        }
+
+                        /** Remove pivot selection */
+                        $(this).removeClass('is-selected');
+                    });
+
+                    /** Add pivot & pivot-content selection */
                     $(this).addClass('is-selected');
+                    $pivotContent.addClass('is-selected');
                 }
 
             });
